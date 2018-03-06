5803c421132a17a54014b456ad67ad1a7d853a7b
Justin Reich <justinreich.dev@gmail.com>
2018-03-06 15:06:57 -0600

    Inheritance and Polymorphism examples

---
+++ src/inheritancePolyLEC/Manager.java
@@ -0,0 +1,7 @@
+package inheritancePolyLEC;
+
+public class Manager extends Employee {
+    public String work() {
+        return "Managing employees";
+    }
+}
---
+++ src/inheritancePolyLEC/Person.java
@@ -0,0 +1,15 @@
+package inheritancePolyLEC;
+
+class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
