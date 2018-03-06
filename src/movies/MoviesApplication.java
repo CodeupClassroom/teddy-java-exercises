9c2a4584a89b82875e05a26342e7c55c883f332d
Zach Gulde <zachgulde@gmail.com>
2018-03-06 10:02:53 -0600

    Start movies application

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,15 @@
+package movies;
+
+/**
+ * 1. Show all the movies
+ * 2. Extract a method
+ * 3. Change our method to handle categories (overload)
+ * 4. De-duplicate
+ * 5. Deal with user input
+ */
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
